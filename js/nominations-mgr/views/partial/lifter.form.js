0813b0f63a257edd1bc423258fba5a5d2f091edb
qa <qa@coaliance.com>
2017-09-25 08:05:29 +0300

    Nom. Man.: validation for juniors competition form

--- js/nominations-mgr/views/partial/lifter.form.js
+++ js/nominations-mgr/views/partial/lifter.form.js
@@ -14,6 +14,27 @@ const LifterForm = (props) => {
     var regionsList = props.regions.map(region => <option key={region.id} value={region.id}>{region.name}</option>);
     var divisions = (nom.gender === "male")? [{value: "open", name: "Відкритий"},{value: "subjuniors", name: "Юнаки"},{value: "juniors", name: "Юніори"},{value: "seniors", name: "Чоловіки"},{value: "masters1", name: "Ветерани 1"},{value: "masters2", name: "Ветерани 2"},{value: "masters3", name: "Ветерани 3"},{value: "masters4", name: "Ветерани 4"}] : 
     [{value: "open", name: "Відкритий"},{value: "subjuniors", name: "Дівчата"},{value: "juniors", name: "Юніорки"},{value: "seniors", name: "Жінки"},{value: "masters1", name: "Ветерани 1"},{value: "masters2", name: "Ветерани 2"},{value: "masters3", name: "Ветерани 3"},{value: "masters4", name: "Ветерани 4"}];
+    var evalAge = (bdate) => {
+        var year = parseInt(new Date().getFullYear());
+        var born = new Date(bdate).getFullYear();
+        var diff = year - born;
+        if(diff >= 12 && diff <=18) return "subjuniors";
+        if(diff>= 19 && diff <= 23) return "juniors";
+        return null;
+    }
+    var validateDivision = () => {
+        if(JSON.parse(info.isJun) && evalAge(bDate)){
+            var isValid = (nom.division === evalAge(bDate))? true : false;
+            return {
+                isValid: isValid,
+                text: <span className="validation">Дивізіон не відповідає віковій групі</span>
+            }
+        }
+        return {
+            isValid: true
+        };
+    }
+
     var divisionsList = divisions.map(div => <option key={div.value} value={div.value}>{div.name}</option>);
     var weightClassList = props.wc.map(w => <option key={w.id} value={w.id}>{w.name}</option>);
     var shortWc = [];
@@ -93,7 +114,7 @@ const LifterForm = (props) => {
                         <td><select value={nom.level} onChange={e => props.onChange("level", e.target.value)}>{levelList}</select></td>
                     </tr>
                     <tr>
-                        <td><label>Дивізіон</label></td>
+                        <td><label>Дивізіон{(!validateDivision().isValid)? validateDivision().text : ""}</label></td>
                         <td><select value={nom.division} onChange={e => props.onChange("division", e.target.value)} >{divisionsList}</select></td>
                     </tr>
                     {ageGroups}
@@ -136,7 +157,7 @@ const LifterForm = (props) => {
             </div>
             <div className="formFooter">
                 <div className="form-footer-tab left">
-                    <button type="button" className="footer-button success" onClick={() => props.onSave()}>Зберегти</button>
+                    <button type="button" className="footer-button success" onClick={() => props.onSave()} disabled={!validateDivision().isValid}>Зберегти</button>
                 </div>
                 <div className="form-footer-tab right">
                 <button type="button" className="footer-button danger" onClick={() => props.onClose()}>Скасувати</button>
