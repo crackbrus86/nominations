0f61ece488453889a515e1112f92e97a4d72b6c9
crackbrus86 <crackbrus86@gmail.com>
2019-03-05 17:33:34 +0200

    competitions sorting

--- api/competitions/GetAllCompetitions.php
+++ api/competitions/GetAllCompetitions.php
@@ -5,7 +5,7 @@ $tb_comp_types = $wpdb->get_blog_prefix()."comp_types";
 $sql = $wpdb->prepare("SELECT $tb_competitions.id AS id, $tb_competitions.name AS name, $tb_competitions.location AS location,
 $tb_competitions.start_date AS startDate, $tb_competitions.end_date AS endDate, $tb_competitions.type AS typeId, $tb_comp_types.name AS typeName, 
 $tb_competitions.is_jun AS isJun, $tb_competitions.is_cup AS isCup FROM $tb_competitions JOIN
-$tb_comp_types ON $tb_competitions.type = $tb_comp_types.id", "");
+$tb_comp_types ON $tb_competitions.type = $tb_comp_types.id ORDER BY startDate", "");
 $competitions = $wpdb->get_results($sql);
 $result = json_encode($competitions);
 print_r($result);
