89a76dba319d43308cde475100fbc32c79e92bfc
crackbrus86 <crackbrus86@gmail.com>
2021-02-27 21:33:07 +0200

    Minor fix

--- js/nominations-adm/views/partial/isJun.lifters.grid.js
+++ js/nominations-adm/views/partial/isJun.lifters.grid.js
@@ -219,7 +219,7 @@ const IsJunLiftersGrid = (props) => {
                             });                                          
                             return (<div key={w.id}>
                                 <div key={w.id} className={classnames('w-class-name', {'warning': w.hide})}>
-                                    {`${w.name}${w.hide && ' !!!Увага!!! Дана категорія є застарілою!'}`}
+                                    {`${w.name}${!!w.hide && ' !!!Увага!!! Дана категорія є застарілою!'}`}
                                 </div>
                                 <Grid data={{columns: gridColumns, rows: items}} />
                             </div>)                                   
