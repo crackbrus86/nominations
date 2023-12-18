ddbe923eee9495fe09ac614cd008ee98929f8167
crackbrus86 <crackbrus86@gmail.com>
2023-12-18 16:14:59 +0200

    Finished Referee Busy component (Save, Load, Refresh, Handle empty flows)

--- js/components/referee.busy/referee.busy.js
+++ js/components/referee.busy/referee.busy.js
@@ -1,22 +1,32 @@
-import { useState, Fragment } from 'react';
+import { useState, Fragment, useEffect } from 'react';
 import moment from 'moment';
+import classNames from 'classnames';
 
 import './referee.busy.scss';
 
 const RefereeBusy = ({ eventId, team, firstName, surname, middleName }) => {
 	const [loading, setLoading] = useState(false);
+	const [showBusy, setShowBusy] = useState(false);
+	const [pending, setPending] = useState(false);
 	const [days, setDays] = useState([]);
 	const [flows, setFlows] = useState([]);
 	const flowsApiUrl = '../wp-content/plugins/nominations/api/flows';
 
-	const loadRefereeBusy = async () => {
+	const refereeId = `${surname.toLowerCase()}/${firstName.toLowerCase()}/${middleName.toLowerCase()}/${team}`;
+
+	const onInitRefereeBusy = async () => {
 		setLoading(true);
+		await loadRefereeBusy();
+		setLoading(false);
+	};
+
+	const loadRefereeBusy = async () => {
 		const response = await fetch(
 			`${flowsApiUrl}/GetRefereeUnavailabilitySchedule?` +
 				new URLSearchParams(
 					{
 						eventId,
-						referee: `${surname.toLowerCase()}/${firstName.toLowerCase()}/${middleName.toLowerCase()}/${team}`,
+						referee: refereeId,
 					},
 					{
 						headers: {
@@ -26,10 +36,38 @@ const RefereeBusy = ({ eventId, team, firstName, surname, middleName }) => {
 				)
 		);
 		const result = await response.json();
-		setDays(result.data.days);
+		const availableDays = result.data.days.filter(
+			(x) => !!result.data.flows.find((f) => f.day_of_flow === x)
+		);
+		setDays(availableDays);
 		setFlows(result.data.flows);
-		console.log(result.data);
-		setLoading(false);
+	};
+
+	const updateRefereeStatus = async (flowId, value) => {
+		if(pending) return;
+		setPending(true);
+		const request = {
+			flowId,
+			refereeId,
+			refereeStatus: value,
+		};
+		await fetch(`${flowsApiUrl}/SaveRefereeRecord`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(request),
+		});
+		setPending(false);
+	};
+
+	useEffect(() => {
+		if (showBusy) onInitRefereeBusy();
+	}, [showBusy]);
+
+	const handleStatusCheckbox = async (flowId, value) => {
+		await updateRefereeStatus(flowId, value);
+		await loadRefereeBusy();
 	};
 
 	const getFlowsOfDay = (day) => {
@@ -48,21 +86,38 @@ const RefereeBusy = ({ eventId, team, firstName, surname, middleName }) => {
 		return dayFlows.findIndex((f) => f.flow_id === flow.flow_id) + 1;
 	};
 
-	const showBusyTable = !loading && !!days.length && !!flows.length;
+	const showBusyTable =
+		showBusy && !loading && !!days.length && !!flows.length;
+	const noFlows = showBusy && !loading && (!days.length || !flows.length);
 
 	return (
 		<Fragment>
-			<button
-				type="button"
-				disabled={!firstName || !surname || !middleName}
-				onClick={loadRefereeBusy}
+			<div
+				id="show-busy-switch"
+				className={classNames({
+					disabled: !firstName || !surname || !middleName,
+				})}
 			>
-				Не можу судити
-			</button>
-			{loading && <div>Loading...</div>}
+				<input
+					type="checkbox"
+					id="showBusySwitch"
+					checked={showBusy}
+					disabled={!firstName || !surname || !middleName}
+					onClick={() => setShowBusy(!showBusy)}
+				/>
+				<label for="showBusySwitch">Показати графік зайнятості</label>
+			</div>
+			{loading && (
+				<div id="referee-busy-preloader">
+					Зачекайте. Йде завантаження...
+				</div>
+			)}
+			{noFlows && (
+				<div>На даний момент ці змагання не розбиті на потоки</div>
+			)}
 			{showBusyTable && (
 				<div id="referee-busy">
-					<table>
+					<table className={classNames({ loading: pending })}>
 						<thead>
 							<tr>
 								{days.map((day, index) => (
@@ -86,6 +141,12 @@ const RefereeBusy = ({ eventId, team, firstName, surname, middleName }) => {
 										<input
 											type="checkbox"
 											checked={flow.busy}
+											onClick={() =>
+												handleStatusCheckbox(
+													flow.flow_id,
+													flow.busy ? 1000 : 0
+												)
+											}
 										/>
 									</td>
 								))}
